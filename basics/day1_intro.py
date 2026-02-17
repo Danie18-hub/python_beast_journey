8a80b29ef75361fafd36e11e9765780ce240a60b
Eyong Daniel <eyongd573@gmail.com>
2026-02-16 17:05:51 -0800

    Hands-On Coding Tasks

---
+++ basics/day1_intro.py
@@ -0,0 +1,6 @@
+print("Welcome to Python Beast Mode!")
+name = input("What's your name?")
+print(f"Hello, {name}! Let's master Python")
+
+age = input("What is your age?")
+print(f"You are {age} years old")
