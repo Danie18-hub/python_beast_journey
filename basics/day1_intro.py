7ece0b1b9c777dd54021e8dd1d9123db767032b1
Eyong Daniel <eyongd573@gmail.com>
2026-02-17 02:18:46 -0800

    Day 2

--- basics/day1_intro.py
+++ basics/day1_intro.py
@@ -4,3 +4,26 @@ print(f"Hello, {name}! Let's master Python")
 
 age = input("What is your age?")
 print(f"You are {age} years old")
+
+
+age = float (input("What is your age?"))
+print(f"You are {age} years old")
+
+matricule = str (input("Enter your matricule"))
+print(f"Your matricule is {matricule}")
+
+x = 42
+y = 3.14
+name = "Alice"
+is_student = True
+fruits = ["apple", "banana"]
+coordinates = (10, 20)
+person = {"name": "Bob", "age":50}
+unique = {1,2,3}
+
+print(unique, type(unique))
+
+#INT TO STRING
+a = 5
+b = float(a)
+print(b ,type(b))
